99297c17c9f6cccaa70b7ffbf3c22328b7f9478a
ritikakhokhar <khokharritika73@gmail.com>
2024-05-29 19:13:04 +1000

    Create session2.py
    session_2 practice

---
+++ session2.py
@@ -0,0 +1,7 @@
+first_thing = input("what is the numeber?:")
+second_thing = input("what is the name?:")
+print(first_thing, second_thing)
+
+what is the numeber?: rrr
+what is the name?: ttt
+rrr ttt
